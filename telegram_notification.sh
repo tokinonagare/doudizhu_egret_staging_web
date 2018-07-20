af9adec5b2ef030fc66f34140836d27f4ffd6ff8
tokinonagare <timemahou@gmail.com>
2018-07-20 20:37:33 +0800

    删除 timestamp 文件

--- telegram_notification.sh
+++ telegram_notification.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-timestamp=$(cat timestamp)
 commit_info=$(cat commit_info)
-curl -v --data-urlencode "text=斗地主 web 部署完成 ##{server_type} ${timestamp} ${commit_info}" "https://api.telegram.org/bot213022846:AAFfsv0sfyffvmZDtNftvMW0gbVimM4O1P8/sendMessage?chat_id=-1001138683387"
+curl -v --data-urlencode "text=斗地主 web 部署完成 ##{server_type} ${commit_info}" "https://api.telegram.org/bot213022846:AAFfsv0sfyffvmZDtNftvMW0gbVimM4O1P8/sendMessage?chat_id=-1001138683387"
